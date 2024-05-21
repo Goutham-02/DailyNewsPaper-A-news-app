a6bee4832516cc1a73d675f06f7b4937bb0c752f
Goutham-02 <gouthamshastry02@gmail.com>
2024-05-21 22:06:23 +0530

    API fetched

--- src/components/News.js
+++ src/components/News.js
@@ -2,23 +2,35 @@ import React, { Component } from 'react'
 import { Newsitem } from './Newsitem'
 
 export class News extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      articles: [],
+      loading: false
+    }
+  }
+
+  async componentDidMount() {
+    let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=d72b059dcd71457d8b8d222ca85a1ab9";
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({ articles: parsedData.articles })
+  }
+
   render() {
     return (
       <div className='container my-3'>
 
         <h2>DailyNewsPaper - Top Headlines</h2>
         <div className="row">
-          <div className="col-md-4">
-            <Newsitem title="myTitle" description="mydesc" imageURL="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg"/>
-          </div>
-          <div className="col-md-4">
-            <Newsitem title="myTitle" description="mydesc" />
-          </div>
-          <div className="col-md-4">
-            <Newsitem title="myTitle" description="mydesc" />
-          </div>
+          {this.state.articles.map((element) => {
+            return <div className="col-md-4" key={element.url}>
+              <Newsitem title={element.title?element.title:""} description={element.description?element.description:""} imageURL={element.urlToImage} newsUrl={element.url} />
+            </div>
+          })}
         </div>
-
       </div>
     )
   }
--- src/components/Newsitem.js
+++ src/components/Newsitem.js
@@ -3,16 +3,16 @@ import React, { Component } from 'react'
 export class Newsitem extends Component {
 
   render() {
-    let { title, description, imageURL } = this.props;
+    let { title, description, imageURL, newsUrl } = this.props;
     return (
       <div>
         <div className="my-3">
           <div className="card" style={{ width: "18rem" }}>
-            <img src={imageURL} className="card-img-top" alt="..." />
+            <img src={imageURL?imageURL:"https://static.toiimg.com/thumb/msid-110277122,imgsize-1089299,width-900,height-1200,resizemode-6/110277122.jpg"} className="card-img-top" alt="..." />
             <div className="card-body">
               <h5 className="card-title">{title}</h5>
               <p className="card-text">{description}</p>
-              <a href="/newsdetail" className="btn btn-sm btn-primary">Read More</a>
+              <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">Read More</a>
             </div>
           </div>
         </div>
