6f43f7137cde8b64cfb31de028e3cfa150702f80
Goutham-02 <gouthamshastry02@gmail.com>
2024-05-24 16:22:48 +0530

    Prev, Next buttons added

--- src/components/News.js
+++ src/components/News.js
@@ -7,31 +7,66 @@ export class News extends Component {
     super();
     this.state = {
       articles: [],
-      loading: false
+      loading: false,
+      page: 1
     }
   }
 
   async componentDidMount() {
-    let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=d72b059dcd71457d8b8d222ca85a1ab9";
+    let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=d72b059dcd71457d8b8d222ca85a1ab9&page=1&pagesize=20";
     let data = await fetch(url);
     let parsedData = await data.json();
     console.log(parsedData);
-    this.setState({ articles: parsedData.articles })
+    this.setState({ articles: parsedData.articles, totalArticles: parsedData.totalResults })
+  }
+
+  handlePrevClick = async () => {
+    console.log("Previous");
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=d72b059dcd71457d8b8d222ca85a1ab9&page=${this.state.page - 1}&pagesize=20`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({
+      page: this.state.page - 1,
+      articles: parsedData.article
+    });
+    console.log(this.state.page);
+  }
+
+  hhandleNextClick = async () => {
+    console.log("Next");
+    if (this.state.page + 1 > Math.ceil(this.state.totalResults / 20)) {
+
+    }
+    else {
+      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=d72b059dcd71457d8b8d222ca85a1ab9&page=${this.state.page + 1}&pagesize=20`;
+      let data = await fetch(url);
+      let parsedData = await data.json();
+      console.log(parsedData)
+      this.setState({
+        page: this.state.page + 1,
+        articles: parsedData.articles
+      });
+      console.log(this.state.page);
+    }
   }
 
   render() {
     return (
       <div className='container my-3'>
-
-        <h2>DailyNewsPaper - Top Headlines</h2>
+        <h1 className="text-center">DailyNewsPaper - Top Headlines</h1>
         <div className="row">
           {this.state.articles.map((element) => {
             return <div className="col-md-4" key={element.url}>
-              <Newsitem title={element.title?element.title:""} description={element.description?element.description:""} imageURL={element.urlToImage} newsUrl={element.url} />
+              <Newsitem title={element.title ? element.title : ""} description={element.description ? element.description : ""} imageURL={element.urlToImage} newsUrl={element.url} />
             </div>
           })}
         </div>
+        <div className="container d-flex justify-content-between">
+          <button disabled={this.state.page <= 1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}>&larr; Previous</button>
+          <button type="button" className="btn btn-dark" onClick={this.hhandleNextClick}>Next &rarr;</button>
+        </div>
       </div>
+
     )
   }
 }
--- src/components/Newsitem.js
+++ src/components/Newsitem.js
@@ -8,11 +8,11 @@ export class Newsitem extends Component {
       <div>
         <div className="my-3">
           <div className="card" style={{ width: "18rem" }}>
-            <img src={imageURL?imageURL:"https://static.toiimg.com/thumb/msid-110277122,imgsize-1089299,width-900,height-1200,resizemode-6/110277122.jpg"} className="card-img-top" alt="..." />
+            <img src={imageURL ? imageURL : "https://static.toiimg.com/thumb/msid-110277122,imgsize-1089299,width-900,height-1200,resizemode-6/110277122.jpg"} className="card-img-top" alt="..." />
             <div className="card-body">
               <h5 className="card-title">{title}</h5>
               <p className="card-text">{description}</p>
-              <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">Read More</a>
+              <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
             </div>
           </div>
         </div>
